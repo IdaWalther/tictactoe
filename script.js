039f40ec9a019170ec07a9bf81be36a50c698a06
Ida Walther <idahwalther@icloud.com>
2024-01-12 11:26:07 +0100

    Added winningCombos

--- script.js
+++ script.js
@@ -94,7 +94,16 @@ function checkForGameOver() {
 //som motsvarar nuvarande combo innehåller playerIn. Om sant, ändra värdet på flaggan.
 //Returnera flaggan isWinner
 function checkWinner(playerIn) {
-
+    let winningCombos = [
+        [0,1,2], 
+        [3,4,5], 
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6],
+    ]
 }
 
 //Kontrollera om alla platser i oGameData.GameField är fyllda. Om sant returnera true, annars false.
